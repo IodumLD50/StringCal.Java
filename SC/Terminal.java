e7d4baae2f91a78da45f0de1ada695cfc361552a
IodumLD50 <113625113+IodumLD50@users.noreply.github.com>
2023-03-03 18:43:03 +0300

    Add files via upload

---
+++ SC/Terminal.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+import static java.lang.System.out;
+
+public class Terminal {
+    static void input(){
+        out.println("Введите вырожение: ");
+    }
+    static void print(String text){
+        out.println("Ответ: ");
+        out.println("\"" + text + "\"");}
+
+}
+
